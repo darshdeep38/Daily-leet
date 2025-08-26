bdd59111077994e6450dbb5ded683a3377037e3f
Navtej968 <108707416+Navtej968@users.noreply.github.com>
2025-08-26 14:43:52 +0530

    Update PartitionList.cpp

--- PartitionList.cpp
+++ PartitionList.cpp
@@ -47,3 +47,44 @@ public:
 
     }
 };
+
+//
+//equivalent java code
+//
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+ 
+        ListNode small = new ListNode(0);
+        ListNode large = new ListNode(0);
+        
+
+        ListNode smallPtr = small;
+        ListNode largePtr = large;
+        
+        while (head != null) {
+            if (head.val < x) {
+                smallPtr.next = head;
+                smallPtr = smallPtr.next;
+            } else {
+                largePtr.next = head;
+                largePtr = largePtr.next;
+            }
+            head = head.next;
+        }
+
+
+        smallPtr.next = large.next;
+        largePtr.next = null; // Important to avoid cycle
+
+        return small.next;
+    }
+}
+
